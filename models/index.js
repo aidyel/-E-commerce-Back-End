8dba532bbfc90b48f71a58dd9b41beed2ab78c61
aidyel <aidayeli@icloud.com>
2022-04-29 09:37:04 -0400

    added models

---
+++ models/index.js
@@ -0,0 +1,33 @@
+// import models
+const Product = require('./Product');
+const Category = require('./Category');
+const Tag = require('./Tag');
+const ProductTag = require('./ProductTag');
+
+// Create associations
+// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: ''
+})
+
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: ''
+})
+
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag
+})
+
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  
+})
+
+module.exports = {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+};
